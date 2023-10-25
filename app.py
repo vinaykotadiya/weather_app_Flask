3ee0431d6c894001586d0dff1857149593177fc3
vinaykotadiya <vinaykotadiya49@gmail.com>
2023-10-25 11:08:04 +0530

    this is show only city

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template ,request
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -7,18 +7,39 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
-@app.route('/weatherapp',methods = ['POST','GET'])
+@app.route('/weatherapp', methods=['POST', 'GET'])
 def get_weatherdata():
-    url = 'https://api.openweathermap.org/data/2.5/weather'
-    param = {
-        'q' :request.form.get('city'),
-        'appid':request.form .get('appid'),
-        'units':request.form.get('units')
-    }
+    if request.method == 'POST':
+        # Get the form data
+        city = request.form.get('city')
+        appid = request.form.get('appid')
+        units = request.form.get('units')
 
-    response=requests.get(url,params=param)
-    data = response.json()
-    return f"data : {data}"
+        # Check if any of the form fields are empty
+        if not city or not appid or not units:
+            return "Please fill out all fields in the form."
+
+        # Make the API request
+        url = 'https://api.openweathermap.org/data/2.5/weather'
+        params = {
+            'q': city,
+            'appid': appid,
+            'units': units
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            # city_name = data.get('name')
+            # temperature = data['main']['temp']
+            # weather_description = data['weather'][0]['description']
+            # return f"Weather in {city_name}: {weather_description}, Temperature: {temperature}°C"
+            return f"city :{city}"
+        else:
+            return "Failed to fetch weather data. Please check your input and API key."
+
+    return "Please submit the form to get weather data."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5002,debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
